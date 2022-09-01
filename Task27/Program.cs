ef3d2824b2877d304ac1e6604f1aae0e3f9f28ab
Viktor <savskij@bk.ru>
2022-09-01 22:26:31 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿int sumNum (int num) {
+int a = 1;
+int res = 0;
+while ( num > 0) {
+a = num%10;
+num = num/10;
+res = res + a;
+}
+return res;
+}
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int sum = sumNum(num);
+Console.WriteLine(sum);
