3e7a28c165d27e7d99bd51c5c74ec65380185d96
Viktor <savskij@bk.ru>
2022-09-01 22:20:28 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿int exponentiation(int A, int B)
+{
+int c = A;
+for (int i = 1; i < B; i++) {
+c = c*A;
+}
+return c;
+}
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+int ex = exponentiation(a, b);
+Console.WriteLine(ex);
